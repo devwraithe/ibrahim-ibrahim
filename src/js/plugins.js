3f8fe9d6b7078be1b0651c4307c790edaf4dd041
Dev Wraithe <folahanmi0001@gmail.com>
2021-08-06 02:52:48 +0100

    initial commit - v1.0.0

---
+++ src/js/plugins.js
@@ -0,0 +1,48 @@
+/* === avoid errors in browsers that lack a console === */
+(function () {
+  var method;
+  var noop = function () {};
+  var methods = [
+    "assert",
+    "clear",
+    "count",
+    "debug",
+    "dir",
+    "dirxml",
+    "error",
+    "exception",
+    "group",
+    "groupCollapsed",
+    "groupEnd",
+    "info",
+    "log",
+    "markTimeline",
+    "profile",
+    "profileEnd",
+    "table",
+    "time",
+    "timeEnd",
+    "timeline",
+    "timelineEnd",
+    "timeStamp",
+    "trace",
+    "warn",
+  ];
+  var length = methods.length;
+  var console = (window.console = window.console || {});
+
+  while (length--) {
+    method = methods[length];
+
+    // Only stub undefined methods.
+    if (!console[method]) {
+      console[method] = noop;
+    }
+  }
+})();
+
+/* === plugin for locomotive scroll === */
+const scroller = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+});
---
+++ versctrl.md
@@ -0,0 +1,3 @@
+Version Control Notes
+=====
+v1.0.0 - 3rd August 2021
